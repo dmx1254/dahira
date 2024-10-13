f0b2d8aa451acf7a04c03dfd5bfec2bf510037a8
dmx1254 <tipox1254@gmail.com>
2024-10-13 18:05:54 +0100

    Changes

--- app/api/ipn-paydunya/route.ts
+++ app/api/ipn-paydunya/route.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 
-
-export async function POST(request: Request){
-    
+export async function POST(request: Request) {
+  if (request.method === "POST") {
+    try {
+      const notification = request.body;
+      return NextResponse.json(notification, { status: 200 });
+    } catch (error: any) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  }
 }
