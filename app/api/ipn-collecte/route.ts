cf7b6a8395299d35bec75d750b054380286f347d
dmx1254 <tipox1254@gmail.com>
2024-10-13 20:05:03 +0100

    Changes

---
+++ app/api/ipn-collecte/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+import CotisationModel from "@/lib/cotisationModel";
+
+export async function POST(request: Request) {
+  if (request.method === "POST") {
+    try {
+      const notification = await request.json();
+      await CotisationModel.create(notification);
+    //   return NextResponse.json(notification, { status: 200 });
+    } catch (error: any) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  }
+}
