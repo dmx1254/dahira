3949b7a909a9f5240ea34f36cc620df3245af4b2
dmx1254 <tipox1254@gmail.com>
2024-10-14 17:46:04 +0100

    Changes

--- app/api/ipn-paydunya/route.ts
+++ app/api/ipn-paydunya/route.ts
@@ -1,15 +1,24 @@
 import { NextResponse } from "next/server";
-
 import CotisationModel from "@/lib/cotisationModel";
 
 export async function POST(request: Request) {
-  if (request.method === "POST") {
-    try {
-      const notification = await request.json();
-      await CotisationModel.create(notification);
-    //   return NextResponse.json(notification, { status: 200 });
-    } catch (error: any) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+  try {
+    // Parse formData, assuming that the request contains form-data
+    const formData = await request.formData();
+
+    // Convert formData to a regular object
+    const notification: any = {};
+    formData.forEach((value, key) => {
+      notification[key] = value;
+    });
+
+    // Create a new entry in the database
+    await CotisationModel.create(notification);
+
+    // Respond with the received notification
+    return NextResponse.json({test: "testing ping"}, { status: 200 });
+  } catch (error: any) {
+    // Handle errors and return a 500 response if something goes wrong
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
